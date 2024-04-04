e08b873e026d63750ddc6b1fe13d952859f8994d
Yee Lam Elim Thompson <116279163+ElimThompsonFDA@users.noreply.github.com>
2024-04-04 10:41:43 -0400

    Accepted '.dat' and '.dat.raw' if only one projection

--- PcTK_Integration/mcgputools/read_mcgpu.m
+++ PcTK_Integration/mcgputools/read_mcgpu.m
@@ -4,11 +4,15 @@ Nz = Nch  ;
 NE = Nbin ;
 
 cd (MCGPU_output_folder)
-numProjections = Nview
+numProjections = Nview;
 tic
 for ii = 0:numProjections-1
     if read_binary(1) == 1
-        filename = strcat (output_filename, sprintf('.dat_%04d.raw',ii));
+        if numProjections == 1
+            filename = strcat (output_filename, sprintf('.dat.raw'));
+        else
+            filename = strcat (output_filename, sprintf('.dat_%04d.raw',ii));
+        end
         cd (home_folder)
         readRaw
         if numel(M) == Nx * Nz * NE
@@ -18,7 +22,11 @@ for ii = 0:numProjections-1
         end
         ii
     else
-        filename = strcat (output_filename, sprintf('.dat_%04d',ii));
+        if numProjections == 1
+            filename = strcat (output_filename, sprintf('.dat'));
+        else
+            filename = strcat (output_filename, sprintf('.dat_%04d',ii));
+        end
         read_here
         if numel(M) == Nx * Nz * NE
            Proj_MCGPU(ii+1,:,:,:) = reshape(M,Nx,Nz,NE);
